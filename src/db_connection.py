eb716a29f00e9b81179d32a5df230110c12918c2
Venla R <venla.e.rantanen@gmail.com>
2024-04-09 20:10:30 +0300

    Fix to database connection

--- src/db_connection.py
+++ src/db_connection.py
@@ -1,12 +1,9 @@
 import sqlite3
-from sqlite3 import Error
 from config import DATABASE_FILE_PATH
+
+connection = sqlite3.connect(DATABASE_FILE_PATH)
+connection.row_factory = sqlite3.Row
 
 
 def get_db_connection():
-    try:
-        connection = sqlite3.connect(DATABASE_FILE_PATH)
-        connection.row_factory = sqlite3.Row
-    except Error as e:
-        print(e)
     return connection
