f286616916bd052f46be93bf9e66e831f0370e67
Venla R <venla.e.rantanen@gmail.com>
2024-04-26 16:29:34 +0300

    Add docstring-documentation for contact_repository and user_repository

--- src/repositories/contact_repository.py
+++ src/repositories/contact_repository.py
@@ -4,12 +4,31 @@ from services.user_service import user_service
 
 
 class ContactRepository:
+    """Luokka joka vastaa kontakteihin liittyvistä tietokantaoperaatioista.
+    """
+
     def __init__(self, connection):
+        """Luokan konstruktori.
+
+        Args: 
+            connection: tietokantayhteyden connection-olio
+        """
+
         self._connection = connection
 
-    def find_all(self, uid):
+    def find_all(self, user_id):
+        """Palauttaa kaikki käyttäjän luomat kontaktit.
+
+        Args:
+            uid: käyttäjän id
+
+        Returns:
+            Palauttaa listan Contact-olioita
+
+        """
+
         cursor = self._connection.cursor()
-        cursor.execute("SELECT * FROM contacts WHERE user = ?", (uid, ))
+        cursor.execute("SELECT * FROM contacts WHERE user = ?", (user_id, ))
         rows = cursor.fetchall()
 
         return [Contact(row["id"],
@@ -22,6 +41,16 @@ class ContactRepository:
                         row["user"]) for row in rows]
 
     def find_one(self, contact_id):
+        """Palauttaa kontaktin annetun id'n perusteella.
+
+        Args:
+            contact_id: haettavan kontaktin id
+
+        Returns:
+            Palauttaa Contact-olion mikäli kontakti löytyy, muuten palauttaa None
+
+        """
+
         cursor = self._connection.cursor()
         cursor.execute("SELECT * FROM contacts WHERE id = ?", (contact_id, ))
         row = cursor.fetchone()
@@ -36,17 +65,39 @@ class ContactRepository:
                        row["user"]) if row else None
 
     def delete_all(self, user_id):
+        """Poistaa kaikki käyttäjän luomat kontaktit.
+
+        Args:
+            user_id: käyttäjän id, jonka luomat kontaktit poistetaan
+
+        """
+
         cursor = self._connection.cursor()
         cursor.execute("DELETE FROM contacts WHERE user = ?", (user_id, ))
         self._connection.commit()
 
     def delete_one(self, contact_id):
+        """Poistaa yhden kontaktin annetun id'n perusteella.
+
+        Args:
+            contact_id: poistettavan kontaktin id 
+        """
+
         cursor = self._connection.cursor()
         cursor.execute(
             "DELETE FROM contacts WHERE id = ?", (contact_id, ))
         self._connection.commit()
 
     def create(self, contact):
+        """Luo uuden kontaktin.
+
+        Args:
+            contact: tallennettava kontakti Contact-oliona
+
+        Returns:
+            Palauttaa luodun kontaktin Contact-oliona.
+
+        """
         cursor = self._connection.cursor()
         cursor.execute(
             "INSERT INTO contacts (id, first_name, last_name, email, phone, role, user)"
--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -3,10 +3,25 @@ from db_connection import get_db_connection
 
 
 class UserRepository:
+    """Luokka joka vastaa käyttäjiin liittyvistä tietokantaoperaatioista.
+    """
+
     def __init__(self, connection):
+        """Luokan konstruktori.
+
+        Args: 
+            connection: tietokantayhteyden connection-olio
+        """
+
         self._connection = connection
 
     def find_all(self):
+        """Palauttaa kaikki käyttäjät.
+
+        Returns:
+            Palauttaa listan User-olioita
+        """
+
         cursor = self._connection.cursor()
         cursor.execute("SELECT * FROM users")
         rows = cursor.fetchall()
@@ -14,6 +29,16 @@ class UserRepository:
         return [User(row["username"], row["password"], row["id"]) for row in rows]
 
     def find_one(self, username):
+        """Palauttaa käyttäjän annetun käyttäjänimen perusteella.
+
+        Args:
+            username: haettavan käyttäjän käyttäjänimi
+
+        Returns:
+            Palauttaa User-olion mikäli käyttäjä löytyy, muuten palauttaa None
+
+        """
+
         cursor = self._connection.cursor()
         cursor.execute("SELECT * FROM users WHERE username = ?", (username, ))
         row = cursor.fetchone()
@@ -21,11 +46,24 @@ class UserRepository:
         return User(row["username"], row["password"], row["id"]) if row else None
 
     def delete_all(self):
+        """Poistaa kaikki käyttäjät. 
+        """
+
         cursor = self._connection.cursor()
         cursor.execute("DELETE FROM users")
         self._connection.commit()
 
     def create(self, user):
+        """Luo uuden käyttäjän.
+
+        Args:
+            user: tallennettava kontakti User-oliona
+
+        Returns:
+            Palauttaa luodun käyttäjän User-oliona.
+
+        """
+
         cursor = self._connection.cursor()
         cursor.execute("INSERT INTO users (id, username, password) VALUES (?, ?, ?)",
                        (user.id, user.username, user.password))
